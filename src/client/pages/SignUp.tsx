867219f697241876d22515aea47b1a738c4ff5df
Avisek Das <avisekdas555@gmail.com>
2024-01-16 15:52:22 +0530

    feat(client): implement user signup and login

--- src/client/pages/SignUp.tsx
+++ src/client/pages/SignUp.tsx
@@ -1,19 +1,72 @@
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../contexts/AuthContext'
+import toast from 'react-hot-toast'
+import { useEffect } from 'react'
+
 export default function SignUp() {
+  const navigate = useNavigate()
+  const auth = useAuth()
+  
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const name = formData.get('name') as string
+    const email = formData.get('email') as string
+    const password = formData.get('password') as string
+    try {
+      toast.loading('Signing Up', { id: 'signup' })
+      await auth?.signup(name, email, password)
+      toast.success('Signed Up Successfully', { id: 'signup' })
+    } catch (error) {
+      console.log(error)
+      toast.error('Signing Up Failed', { id: 'signup' })
+    }
+  }
+  
+  useEffect(() => {
+    if (auth?.user) {
+      return navigate('/chat')
+    }
+  }, [auth])
+  
   return (
     <div className="w-full h-full grid place-content-center">
       
       <div className="w-96">
       <h1 className="mb-3 text-4xl text-center">Create your account</h1>
-        <form className="grid">
+        <form
+          className="grid"
+          onSubmit={handleSubmit}
+        >
           
-        <div className="mb-5">
+          <div className="mb-5">
+            <label className="block px-4 py-3 leading-none" htmlFor="name">Name</label>
+            <input
+              className="w-full px-4 py-3 bg-slate-200 dark:bg-slate-800 rounded-full"
+              type="text"
+              id="name"
+              name="name"
+            />
+          </div>
+          
+          <div className="mb-5">
             <label className="block px-4 py-3 leading-none" htmlFor="email">Email</label>
-            <input type="email" className="w-full px-4 py-3 bg-slate-200 dark:bg-slate-800 rounded-full" id="email"/>
+            <input
+              className="w-full px-4 py-3 bg-slate-200 dark:bg-slate-800 rounded-full"
+              type="email"
+              id="email"
+              name="email"
+            />
           </div>
           
           <div className="mb-5">
             <label className="block px-4 pb-3 leading-none" htmlFor="password">Password</label>
-            <input type="password" className="w-full px-4 py-3 bg-slate-200 dark:bg-slate-800 rounded-full" id="password"/>
+            <input
+              className="w-full px-4 py-3 bg-slate-200 dark:bg-slate-800 rounded-full"
+              type="password"
+              id="password"
+              name="password"
+            />
           </div>
           
           <button
